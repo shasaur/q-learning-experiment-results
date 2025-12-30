db7d14f66187ba994d5c26848228bb0d3ccea390
Shasaur <shasaur@hotmail.co.uk>
2025-12-30 16:02:54 +0100

    add more experiments

--- src/Experiment1.jsx
+++ src/Experiment1.jsx
@@ -18,14 +18,28 @@ import part4data7x7 from './experiment1-part4-7x7-data.json'
 import part4data10x10 from './experiment1-part4-10x10-data.json'
 import part4data15x15 from './experiment1-part4-15x15-data.json'
 
+import part5data from './experiment1-part5-data.json'
+import part6data from './experiment1-part6-data.json'
+
+const equation1 = `target = reward + gamma * np.max(q_table[next_state])
+q_table[state, action] = q_table[state, action] + alpha * (target - q_table[state, action])`
+
 const Experiment1 = () => (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <h2>Experiment 1</h2>
+        <h2>Experiment 1 - Basic Q-Learning</h2>
         <p>A simple experiment for an agent to learn to navigate to food in a map.</p>
         <SyntaxHighlighter language="python" style={oneDark} customStyle={{fontSize: '14px', margin: '10px 0'}}>
-q_table[state, action] = q_table[state, action] + alpha * (reward - q_table[state, action])
+            {equation1}
         </SyntaxHighlighter>
-        <h3>Part 1: Variation in learning with the same hyperparameters</h3>
+        <ul>
+            <li>
+                For every possible state, and for every possible action in that state, we store a value in the q-table.
+            </li>
+            <li>
+                Our reward is not only what we receive at which step, but also the new rewards taking that step will unlock (i.e. what the new actions in that state will give as a reward)
+            </li>
+        </ul>
+        <h3>Part 1.1: Variation in learning with the same hyperparameters</h3>
         <div style={{ width: 500, height: 400, color: 'white' }}>
             <ResponsiveLine /* or Line for fixed dimensions */
                 data={part1data}
@@ -56,7 +70,7 @@ q_table[state, action] = q_table[state, action] + alpha * (reward - q_table[stat
                 ]}
             />
         </div>
-        <h3>Part 2: Averaging 10 runs for more consistent comparisons</h3>
+        <h3>Part 1.2: Averaging 10 runs for more consistent comparisons</h3>
         <div style={{ width: 500, height: 400, color: 'white' }}>
             <ResponsiveLine /* or Line for fixed dimensions */
                 data={part2data}
@@ -87,7 +101,7 @@ q_table[state, action] = q_table[state, action] + alpha * (reward - q_table[stat
                 ]}
             />
         </div>
-        <h3>Part 3: Scaling the difficulty of the task</h3>
+        <h3>Part 1.3: Scaling the difficulty of the task</h3>
         <p>Trying different grid sizes to see how it affects learning. 5x5, 7x7, 10x10, 15x15</p>
         <div
         style={{flexWrap: 'wrap', width: '80vw', flexDirection: 'row', display: 'flex'}}>
@@ -212,8 +226,26 @@ q_table[state, action] = q_table[state, action] + alpha * (reward - q_table[stat
                 />
             </div>
         </div>
-        <h3>Part 4: Using deep q-learning</h3>
+        <h3>Conclusions</h3>
+        <p>
+            Tabular Q-learning is bad at generalizing to unknown situations and hence scaling learning to larger grid sizes takes exponentially longer.
+            This is my impression, however, it could be tested as a standalone experiment where some positions are deliberately not included in the training set.
+        </p>
+        <h2>Experiment 2 - Deep Q-Learning</h2>
         <p>Trying out deep q-learning and comparing the performance of against standard tabular q-learning.</p>
+        <ul>
+            <li>The table is replaced with a function / network</li>
+            <li><b>Target network:</b> needed to stabilise learning because a Q-network tries to predict both the current and future rewards, which can cause rapid shifting in rewards. A target network is essentially a copy of the Q-network that is updated less frequently. Without target networks, DQN often fails to learn effective policies, as the Q-value estimates diverge due to rapidly shifting targets.</li>
+            <li><b>Experience replay buffer:</b> tracks all the actions taken and the rewards received</li>
+        </ul>
+        <p>Useful resources:</p>
+        <ul>
+            <li><a href="https://milvus.io/ai-quick-reference/what-are-target-networks-in-dqn">Target networks</a></li>
+            <li><a href="https://www.youtube.com/watch?v=x83WmvbRa2I">Deep Q-Learning</a></li>
+        </ul>
+        <h3>Part 2.1: Comparing Deep Q-Learning with Tabular Q-Learning</h3>
+        <p>Grid sizes: 5x5, 7x7, 10x10, 15x15</p>
+        <p>Number of layers: 3. Number of neurons in the hidden layer: 128.</p>
         <p>Commit ID: a4af5cec85fd9cb0c316390392f0c19520b023ae</p>
         <div style={{flexWrap: 'wrap', width: '80vw', flexDirection: 'row', display: 'flex'}}>
             <div style={{ width: 500, height: 400, color: 'white' }}>
@@ -336,6 +368,70 @@ q_table[state, action] = q_table[state, action] + alpha * (reward - q_table[stat
                     ]}
                 />
             </div>
+        </div>
+
+        <h3>Part 2.2: Varying the number of layers and neurons in the hidden layer</h3>
+        <p>Grid size: 10x10. Hidden layers: 1.</p>
+        <div style={{ width: 500, height: 400, color: 'white' }}>
+            <ResponsiveLine /* or Line for fixed dimensions */
+                data={part5data}
+                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                yScale={{ type: 'linear', min: 'auto', max: 'auto', reverse: false }}
+                axisBottom={{ 
+                    legend: 'Episodes', 
+                    legendOffset: 36,
+                    tickValues: [3000, 6000, 9000, 12000, 15000, 18000, 20000],
+                }}
+                axisLeft={{ legend: 'Average Steps per Episode', legendOffset: -40 }}
+                pointSize={0}
+                pointColor={{ theme: 'background' }}
+                pointBorderWidth={2}
+                pointBorderColor={{ from: 'seriesColor' }}
+                pointLabelYOffset={-12}
+                enableTouchCrosshair={true}
+                useMesh={true}
+                legends={[
+                    {
+                        anchor: 'bottom-right',
+                        direction: 'column',
+                        translateX: 100,
+                        itemWidth: 80,
+                        itemHeight: 22,
+                        symbolShape: 'circle'
+                    }
+                ]}
+            />
+        </div>
+
+        <div style={{ width: 500, height: 400, color: 'white' }}>
+            <ResponsiveLine /* or Line for fixed dimensions */
+                data={part6data}
+                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                yScale={{ type: 'linear', min: 'auto', max: 'auto', reverse: false }}
+                axisBottom={{ 
+                    legend: 'Episodes', 
+                    legendOffset: 36,
+                    tickValues: [3000, 6000, 9000, 12000, 15000, 18000, 20000],
+                }}
+                axisLeft={{ legend: 'Average Steps per Episode', legendOffset: -40 }}
+                pointSize={0}
+                pointColor={{ theme: 'background' }}
+                pointBorderWidth={2}
+                pointBorderColor={{ from: 'seriesColor' }}
+                pointLabelYOffset={-12}
+                enableTouchCrosshair={true}
+                useMesh={true}
+                legends={[
+                    {
+                        anchor: 'bottom-right',
+                        direction: 'column',
+                        translateX: 100,
+                        itemWidth: 80,
+                        itemHeight: 22,
+                        symbolShape: 'circle'
+                    }
+                ]}
+            />
         </div>
     </div>
 )
